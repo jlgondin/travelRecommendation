2c38195a507b1e574b227eb1749d7ff006402c04
jlgondin <jeanloup.gondin@orange.com>
2025-10-21 06:16:33 -0400

    travelRecommendation

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,8 +1,4 @@
-const report = document.getElementById("report");
 const btnSearch = document.getElementById('btnSearch');
-const patients = [];
-
-
 
 
 function searchCondition() {
@@ -13,19 +9,12 @@ function searchCondition() {
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+        const condition = data.countries.find(item => item.cities.name.toLowerCase() === input);
 
         if (condition) {
-            const symptoms = condition.symptoms.join(', ');
-            const prevention = condition.prevention.join(', ');
-            const treatment = condition.treatment;
-
             resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-            resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-            resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-            resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            resultDiv.innerHTML += `<p>${condition.description}</p>`;
+            resultDiv.innerHTML += `<img src="${condition.imageUrl}" alt="image">`;
         } else {
             resultDiv.innerHTML = 'Condition not found.';
         }
@@ -37,43 +26,4 @@ function searchCondition() {
 }
 btnSearch.addEventListener('click', searchCondition);
 
-function generateReport() {
-    const numPatients = patients.length;
-    const conditionsCount = {
-    Diabetes: 0,
-    Thyroid: 0,
-    "High Blood Pressure": 0,
-    };
-    const genderConditionsCount = {
-    Male: {
-        Diabetes: 0,
-        Thyroid: 0,
-        "High Blood Pressure": 0,
-    },
-    Female: {
-        Diabetes: 0,
-        Thyroid: 0,
-        "High Blood Pressure": 0,
-    },
-    };
-
-    for (const patient of patients) {
-    conditionsCount[patient.condition]++;
-    genderConditionsCount[patient.gender][patient.condition]++;
-    }
-
-    report.innerHTML = `Number of patients: ${numPatients}<br><br>`;
-    report.innerHTML += `Conditions Breakdown:<br>`;
-    for (const condition in conditionsCount) {
-    report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
-    }
-
-    report.innerHTML += `<br>Gender-Based Conditions:<br>`;
-    for (const gender in genderConditionsCount) {
-    report.innerHTML += `${gender}:<br>`;
-    for (const condition in genderConditionsCount[gender]) {
-        report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[gender][condition]}<br>`;
-    }
-    }
-}
 
